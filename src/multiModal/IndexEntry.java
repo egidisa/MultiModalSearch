a5a3eb3de6ab29b68c82f8cb095da8c7519b15e9
Alessio Villardita <villardita.alessio@gmail.com>
2016-07-10 09:34:46 +0200

    Added getters/setters to IndexEntry.

--- src/multiModal/IndexEntry.java
+++ src/multiModal/IndexEntry.java
@@ -1,13 +1,14 @@
 package multiModal;
 
 public class IndexEntry {
-    public String id;
-    public String tags;
-    public String imgDesc;
-    public String classLabel;
-    public String score;
+    private String id;
+    private String tags;
+    private String imgDesc;
+    private String classLabel;
+    private String score;
 
-    public IndexEntry(String id, String tags, String imgDesc, String classLabel) {
+
+	public IndexEntry(String id, String tags, String imgDesc, String classLabel) {
     	this.id = id;
     	this.tags = tags;
     	this.imgDesc = imgDesc;
@@ -21,4 +22,44 @@ public class IndexEntry {
         this.classLabel = classLabel;
         this.score = score;
     }
+    
+    public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getImgDesc() {
+		return imgDesc;
+	}
+
+	public void setImgDesc(String imgDesc) {
+		this.imgDesc = imgDesc;
+	}
+
+	public String getClassLabel() {
+		return classLabel;
+	}
+
+	public void setClassLabel(String classLabel) {
+		this.classLabel = classLabel;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
 }
